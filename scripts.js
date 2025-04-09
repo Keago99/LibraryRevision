c48e6bf4f7891be2708342c13ced36a28a0b7335
Keago99 <keatabde@gmail.com>
2025-04-09 23:28:06 +0200

    Add Table with correct spacing, add border.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 
+const myLibrary = [];
+
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-
     this.info = function() {
         let infoString = (title + " by " + author + ", " + pages + ", " + read);
         return infoString;
     }
 }
 
-const TheHobbit = new Book("The Hobbit", "J.R.R Tolkien", 322, "Not read yet");
+function addBookToLibrary(){
 
-console.log(TheHobbit.info());
+}
+
+const TheHobbit = new Book("The Hobbit", "J.R.R Tolkien", 322, "Not read yet");
